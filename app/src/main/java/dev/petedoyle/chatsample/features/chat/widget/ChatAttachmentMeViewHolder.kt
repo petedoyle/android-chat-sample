d659ea70d5fcd6c2583daf9e2584c6ae1f826ab5
Pete Doyle <petedoyle@gmail.com>
2019-04-14 19:08:21 -0700

    Load attachment images

--- app/src/main/java/dev/petedoyle/chatsample/features/chat/widget/ChatAttachmentMeViewHolder.kt
+++ app/src/main/java/dev/petedoyle/chatsample/features/chat/widget/ChatAttachmentMeViewHolder.kt
@@ -1,13 +1,20 @@
 package dev.petedoyle.chatsample.features.chat.widget
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import dev.petedoyle.chatsample.databinding.ItemChatAttachmentMeBinding
 import dev.petedoyle.chatsample.features.chat.persistence.ChatItem
 
 class ChatAttachmentMeViewHolder(val binding: ItemChatAttachmentMeBinding) : RecyclerView.ViewHolder(binding.root) {
-
     fun bindTo(attachment: ChatItem.Attachment) {
         binding.attachment = attachment
         binding.executePendingBindings()
+
+        Glide.with(binding.root)
+            .load(attachment.thumbnailUrl)
+            .centerCrop()
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.image)
     }
 }
--- app/src/main/java/dev/petedoyle/chatsample/features/chat/widget/ChatAttachmentViewHolder.kt
+++ app/src/main/java/dev/petedoyle/chatsample/features/chat/widget/ChatAttachmentViewHolder.kt
@@ -1,6 +1,8 @@
 package dev.petedoyle.chatsample.features.chat.widget
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import dev.petedoyle.chatsample.databinding.ItemChatAttachmentBinding
 import dev.petedoyle.chatsample.features.chat.persistence.ChatItem
 
@@ -9,5 +11,11 @@ class ChatAttachmentViewHolder(val binding: ItemChatAttachmentBinding) : Recycle
     fun bindTo(attachment: ChatItem.Attachment) {
         binding.attachment = attachment
         binding.executePendingBindings()
+
+        Glide.with(binding.root)
+            .load(attachment.thumbnailUrl)
+            .centerCrop()
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.image)
     }
 }
