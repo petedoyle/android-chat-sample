89b042b0f01197006d0f04d6aeba911078dc7a9e
Pete Doyle <petedoyle@gmail.com>
2019-04-15 01:14:06 -0700

    Move binding of ChatViewModel to ChatModule

--- app/src/main/java/dev/petedoyle/chatsample/di/ViewModelModule.kt
+++ app/src/main/java/dev/petedoyle/chatsample/di/ViewModelModule.kt
@@ -1,20 +1,13 @@
 package dev.petedoyle.chatsample.di
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
-import dagger.multibindings.IntoMap
-import dev.petedoyle.chatsample.features.chat.ChatViewModel
 
-@Suppress("unused")
 @Module
 abstract class ViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(ChatViewModel::class)
-    abstract fun bindChatViewModel(chatViewModel: ChatViewModel): ViewModel
 
+    @Suppress("unused")
     @Binds
     abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
--- app/src/main/java/dev/petedoyle/chatsample/features/chat/ChatModule.kt
+++ app/src/main/java/dev/petedoyle/chatsample/features/chat/ChatModule.kt
@@ -1,13 +1,24 @@
 package dev.petedoyle.chatsample.features.chat
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
+import dev.petedoyle.chatsample.di.ViewModelKey
 import dev.petedoyle.chatsample.features.chat.paging.ChatDataSourceFactory
 import dev.petedoyle.chatsample.persistence.AppDatabase
 import dev.petedoyle.daggerutil.ActivityScope
 
 @Module
 abstract class ChatModule {
+
+    @Suppress("unused")
+    @ActivityScope
+    @Binds
+    @IntoMap
+    @ViewModelKey(ChatViewModel::class)
+    abstract fun bindChatViewModel(chatViewModel: ChatViewModel): ViewModel
 
     @Module
     companion object {
